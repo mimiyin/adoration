38fe6bb8a7d0ce0fa139f4f66a76a43b617ed56c
mimiyin <mimiyin@gmail.com>
2020-05-31 19:41:00 -0400

    Adding https redirect.

--- server.js
+++ server.js
@@ -1,145 +1,159 @@
-// Create server
-let port = process.env.PORT || 8000;
-let enforce = require('express-sslify');
-let express = require("express");
-let app = express();
+var express = require('express');
+var http = require('http');
+var enforce = require('express-sslify');
 
-// Redirect to HTTPS
-//app.use(enforce.HTTPS());
+var app = express();
 
-// Tell server where to look for files
-app.use(express.static('public'));
+// Use enforce.HTTPS({ trustProtoHeader: true }) in case you are behind
+// a load balancer (e.g. Heroku). See further comments below
+app.use(enforce.HTTPS());
 
-// Create seruver
-let server = require("http")
-  .createServer(app)
-  .listen(port, function() {
-    console.log("Server listening at port: ", port);
-  });
-
-// Create socket connection
-let io = require("socket.io").listen(server);
-
-// Server side data
-let start = false;
-
-// Clients in the output namespace
-let performers = io.of("/performer");
-// Listen for output clients to connect
-performers.on("connection", socket => {
-  console.log("A performer client connected: " + socket.id);
-
-  // Listen for this output client to disconnect
-  socket.on("disconnect", () => {
-    console.log("A performer client has disconnected " + socket.id);
-  });
+http.createServer(app).listen(app.get('port'), function() {
+    console.log('Express server listening on port ' + app.get('port'));
 });
 
-// Clients in the output namespace
-let voices = io.of("/voice");
-// Listen for output clients to connect
-voices.on("connection", socket => {
-  console.log("A voice client connected: " + socket.id);
-
-  // Give start status
-  socket.on("get start", () => {
-    // Sent recording status
-    socket.emit('start', !start);
-  });
-
-  // Listen for data messages from this client
-  socket.on("data", data => {
-    // Data comes in as whatever was sent, including objects
-    //console.log("Received: 'data' " + data);
-    performers.emit("data", data);
-  });
-
-  // Listen for this output client to disconnect
-  socket.on("disconnect", () => {
-    console.log("A voice client has disconnected " + socket.id);
-  });
-});
-
-// Clients in the output namespace
-let conductors = io.of("/conductor");
-// Listen for output clients to connect
-conductors.on("connection", socket => {
-  console.log("A conductor client connected: " + socket.id);
-
-  // Give start status
-  socket.on("get start", () => {
-    // Sent recording status
-    socket.emit('start', start);
-  });
-
-  // Pass on request to record
-  socket.on("start", _start => {
-    console.log("start!");
-    start = _start;
-    // Turn on audience
-    inputs.emit("start", start);
-    // Turn on conductors
-    conductors.emit("start", start);
-    // Turn off voice performer
-    voices.emit("start", !start);
-  });
-
-  // Communicate with performer
-  socket.on("range", range => {
-    performers.emit("range", range);
-  });
-  socket.on("rate", rate => {
-    performers.emit("rate", rate);
-  });
-  // Pass on request to crop
-  socket.on("crop", crop => {
-    performers.emit("crop", crop);
-  });
-  socket.on("freeze", freeze => {
-    performers.emit("freeze", freeze);
-  });
-  socket.on("auto", auto => {
-    performers.emit("auto", auto);
-  });
-
-  // Listen for this output client to disconnect
-  socket.on("disconnect", () => {
-    console.log("A conductor client has disconnected " + socket.id);
-  });
-});
-
-// Clients in the input namespace
-let inputs = io.of("/input");
-// Listen for input clients to connect
-inputs.on("connection", socket => {
-  console.log("An input client connected: " + socket.id);
-
-  // Give start status
-  socket.on("get start", () => {
-    // Sent recording status
-    socket.emit('start', start);
-  });
-
-  // Listen for data messages from this client
-  socket.on("data", data => {
-    // Data comes in as whatever was sent, including objects
-    //console.log("Received: 'data' " + data);
-
-    // Package up data with socket's id
-    let message = {
-      id: socket.id,
-      data: data
-    };
-
-    // Send it to all of the output clients
-    conductors.emit("message", message);
-    performers.emit("data", data);
-  });
-
-  // Listen for this input client to disconnect
-  // Tell all of the output clients this client disconnected
-  socket.on("disconnect", () => {
-    console.log("An input client has disconnected " + socket.id);
-    conductors.emit("disconnected", socket.id);
-  });
-});
+// // Create server
+// let port = process.env.PORT || 8000;
+// let enforce = require('express-sslify');
+// let express = require("express");
+// let app = express();
+//
+// // Redirect to HTTPS
+// //app.use(enforce.HTTPS());
+//
+// // Tell server where to look for files
+// app.use(express.static('public'));
+//
+// // Create seruver
+// let server = require("http")
+//   .createServer(app)
+//   .listen(port, function() {
+//     console.log("Server listening at port: ", port);
+//   });
+//
+// // Create socket connection
+// let io = require("socket.io").listen(server);
+//
+// // Server side data
+// let start = false;
+//
+// // Clients in the output namespace
+// let performers = io.of("/performer");
+// // Listen for output clients to connect
+// performers.on("connection", socket => {
+//   console.log("A performer client connected: " + socket.id);
+//
+//   // Listen for this output client to disconnect
+//   socket.on("disconnect", () => {
+//     console.log("A performer client has disconnected " + socket.id);
+//   });
+// });
+//
+// // Clients in the output namespace
+// let voices = io.of("/voice");
+// // Listen for output clients to connect
+// voices.on("connection", socket => {
+//   console.log("A voice client connected: " + socket.id);
+//
+//   // Give start status
+//   socket.on("get start", () => {
+//     // Sent recording status
+//     socket.emit('start', !start);
+//   });
+//
+//   // Listen for data messages from this client
+//   socket.on("data", data => {
+//     // Data comes in as whatever was sent, including objects
+//     //console.log("Received: 'data' " + data);
+//     performers.emit("data", data);
+//   });
+//
+//   // Listen for this output client to disconnect
+//   socket.on("disconnect", () => {
+//     console.log("A voice client has disconnected " + socket.id);
+//   });
+// });
+//
+// // Clients in the output namespace
+// let conductors = io.of("/conductor");
+// // Listen for output clients to connect
+// conductors.on("connection", socket => {
+//   console.log("A conductor client connected: " + socket.id);
+//
+//   // Give start status
+//   socket.on("get start", () => {
+//     // Sent recording status
+//     socket.emit('start', start);
+//   });
+//
+//   // Pass on request to record
+//   socket.on("start", _start => {
+//     console.log("start!");
+//     start = _start;
+//     // Turn on audience
+//     inputs.emit("start", start);
+//     // Turn on conductors
+//     conductors.emit("start", start);
+//     // Turn off voice performer
+//     voices.emit("start", !start);
+//   });
+//
+//   // Communicate with performer
+//   socket.on("range", range => {
+//     performers.emit("range", range);
+//   });
+//   socket.on("rate", rate => {
+//     performers.emit("rate", rate);
+//   });
+//   // Pass on request to crop
+//   socket.on("crop", crop => {
+//     performers.emit("crop", crop);
+//   });
+//   socket.on("freeze", freeze => {
+//     performers.emit("freeze", freeze);
+//   });
+//   socket.on("auto", auto => {
+//     performers.emit("auto", auto);
+//   });
+//
+//   // Listen for this output client to disconnect
+//   socket.on("disconnect", () => {
+//     console.log("A conductor client has disconnected " + socket.id);
+//   });
+// });
+//
+// // Clients in the input namespace
+// let inputs = io.of("/input");
+// // Listen for input clients to connect
+// inputs.on("connection", socket => {
+//   console.log("An input client connected: " + socket.id);
+//
+//   // Give start status
+//   socket.on("get start", () => {
+//     // Sent recording status
+//     socket.emit('start', start);
+//   });
+//
+//   // Listen for data messages from this client
+//   socket.on("data", data => {
+//     // Data comes in as whatever was sent, including objects
+//     //console.log("Received: 'data' " + data);
+//
+//     // Package up data with socket's id
+//     let message = {
+//       id: socket.id,
+//       data: data
+//     };
+//
+//     // Send it to all of the output clients
+//     conductors.emit("message", message);
+//     performers.emit("data", data);
+//   });
+//
+//   // Listen for this input client to disconnect
+//   // Tell all of the output clients this client disconnected
+//   socket.on("disconnect", () => {
+//     console.log("An input client has disconnected " + socket.id);
+//     conductors.emit("disconnected", socket.id);
+//   });
+// });
