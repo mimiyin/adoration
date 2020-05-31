09a161aacebf35653782c63107af4e9a9e0cdfb3
mimiyin <mimiyin@gmail.com>
2020-05-31 14:40:38 -0400

    Adding https redirect.

--- server.js
+++ server.js
@@ -3,14 +3,16 @@ let port = process.env.PORT || 8000;
 let enforce = require('express-sslify');
 let express = require("express");
 let app = express();
+
+// Tell server where to look for files
+app.use([enforce.HTTPS(), express.static("public")]);
+
+// Create seruver
 let server = require("http")
   .createServer(app)
   .listen(port, function() {
     console.log("Server listening at port: ", port);
   });
-
-// Tell server where to look for files
-app.use([enforce.HTTPS(), express.static("public")]);
 
 // Create socket connection
 let io = require("socket.io").listen(server);
