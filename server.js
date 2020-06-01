fc478b24183e23cc1b3189dbb86e9d8b28981864
mimiyin <mimiyin@gmail.com>
2020-05-31 20:54:41 -0400

    Adding https redirect.

--- server.js
+++ server.js
@@ -7,21 +7,21 @@ let app = express();
 // Redirect to HTTPS
 //app.use(enforce.HTTPS());
 
-app.use(
-  function(req, res, next) {
-    console.log("SECURE?", req.headers['x-forwarded-proto']);
-    console.log("HI", req.subdomains, req.hostname, req.originalUrl);
-    // if (environments.indexOf(process.env.NODE_ENV) >= 0) {
-    //   if (req.headers['x-forwarded-proto'] != 'https') {
-    //     console.log('NOT SECURE');
-    //     res.redirect(status, 'https://' + req.subdomains[0] + req.hostname + req.originalUrl);
-    //   } else {
-    //     next();
-    //   }
-    // } else {
-    //   next();
-    // }
-  });
+// app.use(
+//   function(req, res, next) {
+//     console.log("SECURE?", req.headers['x-forwarded-proto']);
+//     console.log("HI", req.subdomains, req.hostname, req.originalUrl);
+//     // if (environments.indexOf(process.env.NODE_ENV) >= 0) {
+//     //   if (req.headers['x-forwarded-proto'] != 'https') {
+//     //     console.log('NOT SECURE');
+//     //     res.redirect(status, 'https://' + req.subdomains[0] + req.hostname + req.originalUrl);
+//     //   } else {
+//     //     next();
+//     //   }
+//     // } else {
+//     //   next();
+//     // }
+//   });
 
 // Tell server where to look for files
 app.use(express.static('public'));
