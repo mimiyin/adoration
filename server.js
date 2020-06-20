bde9d8887746f46946fa637e033ba38db0625d73
mimiyin <mimiyin@gmail.com>
2020-06-19 21:40:39 -0400

    fixing random time

--- server.js
+++ server.js
@@ -174,7 +174,7 @@ audience.on("connection", socket => {
     ushers.emit("message", message);
 
     // Send to conductors 20 seconds later
-    let delay = Math.floor(Math.random() * 5) * 20 * 1000;
+    let delay = 20 * 1000; //Math.floor(Math.random() * 5) * 20 * 1000;
     setTimeout(()=>{
       conductors.emit("message", message);
     }, delay);
